1a5ebd1c71e871744aaf9888d17837f5de750f41
Orlando Nelson <nelson8223@gmail.com>
2023-01-08 18:13:32 -0500

    add a 5 day API

--- assets/script.js
+++ assets/script.js
@@ -10,11 +10,11 @@ function getWeather() {}
 
 button.addEventListener("click", function (event) {
   event.preventDefault;
-  console.log(inputValue.value);
+  // console.log(inputValue.value);
   var city = inputValue.value;
 
   function renderWeather(weather) {
-    console.log(weather);
+    // console.log(weather);
    // Pulling the same day Weather forecast API.
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -23,7 +23,7 @@ button.addEventListener("click", function (event) {
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
         
 
         var city = document.createElement("p");
@@ -39,7 +39,7 @@ button.addEventListener("click", function (event) {
         resultsContainer.append(humidity);
 
         var wind = document.createElement("p");
-        wind.textContent = "Wind:" + data.weather.wind + "MPH";
+        wind.textContent = "Wind Speed:" + data.weather.wind + "MPH";
         resultsContainer.append(wind);
 
 
@@ -48,9 +48,55 @@ button.addEventListener("click", function (event) {
         localStorage.setItem("Wind", wind)
         localStorage.setItem("Humidity", humidity)
 
-      
+      dailyWeather(data)
       });
   }
+function dailyWeather(forecast){
+console.log(forecast)
+var lat=forecast.coord.lat
+console.log(lat,"hey")
+var lon=forecast.coord.lon
+
+fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon  
+ + "&appid=65ea356fac868b7989b751cef0bfce08&units=imperial")
+ .then((response) => response.json())
+ .then((data) => {
+console.log(data,"sec fetch")
+
+
+console.log(temp)
+const forecastBox = document.getElementById("weather1")
+forecastBox.innerHTML= ""
+for (let i = 0; i < 5; i++) {
+  var temp = data.list[i].main.temp
+  console.log(temp,"hello world")
+ const tempEl = document.createElement("p")
+//  tempEl.textContent = temp
+ forecastBox.appendChild(tempEl)
+
+ const dateEl = document.createElement("p")
+ dateEl.textContent = data.list[i].dt_txt.substring(0,10)+":"; 
+
+  var tempE1 = document.createElement("p");
+ tempE1.textContent= data.list[i].main.temp + "°F";"fa-sharp fa-solid fa-sun-cloud"
+//  tempE1.classList.add("test")
+  
+
+const dailyBox = document.createElement("div")
+dailyBox.setAttribute("class", "test")
+forecastBox.append(tempE1)
+
+ dailyBox.append(dateEl);
+ dailyBox.append(tempE1);
+
+ forecastBox.append(dailyBox)
+
+
+}
+ }) 
+}
+
+
   renderWeather()
 });
 
